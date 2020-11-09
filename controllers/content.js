ad5327a9624f5b04ae791fc91fb6fcae0668a569
Yi SoJeong <soj980312@gmail.com>
2020-11-09 16:01:01 +0900

    [feat] #2 - 관광지 정보 상세보기
    - contentId를 통해 관광지 상세정보 조회

--- controllers/content.js
+++ controllers/content.js
@@ -81,4 +81,36 @@ const getLocationList = (BASE_URL, API_KEY) => async (req, res, next) => {
   });
 }
 
-module.exports = { getAreaCode, getAreaList, getLocationList };
+const getDetailInfo = (BASE_URL, API_KEY) => async (req, res, next) => {
+  const result = { data:[] };
+  const { id } = req.params;
+  axios.get(`${BASE_URL}/detailCommon?ServiceKey=${API_KEY}&MobileOS=AND&MobileApp=love-korea&_type=json&numOfRows=${AREALIST_NUM}&contentId=${id}&defaultYN=Y&firstImageYN=Y&areacodeYN=Y&addrinfoYN=Y&mapinfoYN=Y&overviewYN=Y`)
+  .then(
+    (response) => {
+      const info = response.data.response.body.items.item;
+      result.data = {
+        contentid: info.contentid,
+        homepage: info.homepage || '',
+        tel: info.tel || '',
+        telname: info.telname || '',
+        title: info.title,
+        firstimage: info.firstimage || '',
+        firstimage2: info.firstimage2 || '',
+        areacode: info.areacode || '',
+        addr1: info.addr1 || '',
+        addr2: info.addr2 || '',
+        zipcode: info.zipcode || '',
+        mapx: info.mapx || '',
+        mapy: info.mapy || '',
+        mlevel: info.mlevel || '',
+        overview: info.overview || ''
+      };
+      console.log(result);
+      res.status(200).json(result);
+    }
+  ).catch((error) => {
+    res.status(400).json({ "message": "해당 관광지 정보를 불러올 수 없습니다." });
+  });
+}
+
+module.exports = { getAreaCode, getAreaList, getLocationList, getDetailInfo };
