89d6d11ac40b6b0c37310d04e67ba7bba7370742
Yi SoJeong <soj980312@gmail.com>
2020-11-09 13:32:28 +0900

    [feat] #2 - 지역기반 관광정보 조회

--- controllers/content.js
+++ controllers/content.js
@@ -1,11 +1,10 @@
 const axios = require('axios');
 
 const AREA_NUM = 17;
+const AREALIST_NUM = 24931
 
 const getAreaCode = (BASE_URL, API_KEY) => async (req, res, next) => {
-  const result = {
-    data: [],
-  };
+  const result = { data: [] };
 
   axios
     .get(
@@ -15,11 +14,40 @@ const getAreaCode = (BASE_URL, API_KEY) => async (req, res, next) => {
       const AreaCodeArr = response.data.response.body.items.item;
       AreaCodeArr.forEach((x) => delete x.rnum);
       result.data = AreaCodeArr;
-      res.json(result);
+      res.status(200).json(result);
     })
     .catch((error) => {
-      console.log(error);
+      res.status(400).json({ "message": "지역을 불러올 수 없습니다." });
     });
 };
 
-module.exports = { getAreaCode };
+const getAreaList = (BASE_URL, API_KEY) => async (req, res, next) => {
+  const result = { data:[] };
+  const areaCode = req.params.id;
+  axios.get(`${BASE_URL}/areaBasedList?ServiceKey=${API_KEY}&MobileOS=AND&MobileApp=love-korea&_type=json&numOfRows=${AREALIST_NUM}&areaCode=${areaCode}`)
+  .then(
+    (response) => {
+      const AreaList = response.data.response.body.items.item;
+      result.data = AreaList.map((x) => {
+        return {
+          addr1: x.addr1 || '',
+          addr2: x.addr2 || '',
+          contentid: x.contentid || '',
+          firstimage: x.firstimage || '',
+          firstimage2: x.firstimage2 || '',
+          mapx: x.mapx || '',
+          mapy: x.mapy || '',
+          mlevel: x.mlevel || '',
+          readcount: x.readcount || '',
+          title: x.title || '',
+          zipcode: x.zipcode || ''
+        }
+      });
+      res.status(200).json(result);
+    }
+  ).catch((error) => {
+    res.status(400).json({ "message": "선택한 지역의 관광지를 불러올 수 없습니다." });
+  });
+}
+
+module.exports = { getAreaCode, getAreaList };
--- routes/korean.js
+++ routes/korean.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getAreaCode } = require('../controllers/content');
+const { getAreaCode, getAreaList } = require('../controllers/content');
 require('dotenv').config();
 
 const BASE_URL = 'http://api.visitkorea.or.kr/openapi/service/rest/KorService';
 const API_KEY = process.env.KO_TOUR_API;
 
 router.get('/area-code', getAreaCode(BASE_URL, API_KEY));
+router.get('/area-list/:id', getAreaList(BASE_URL, API_KEY));
 
 module.exports = router;
